da36c4fb8816271e5392d592117f1f794a6052bc
mkoosinski <72337644+mkoosinski@users.noreply.github.com>
2021-01-10 23:37:52 +0100

    Update script.js

--- 4.Pętle i ify/script.js
+++ 4.Pętle i ify/script.js
@@ -1,32 +1,44 @@
-// Zadanie 1
-//Wypisuj w konsoli co 3 sekundy napis "Cześć po raz " oraz liczbę powtóżeń. Jeśli Licznik dojdzie do 15 zatrzymaj go.
-
-let i = 1;
-    let myTimer = setInterval(() => {
-        console.log("Cześć po raz: " + i);
-        i++;
-        if (i > 15) {
-            clearInterval(myTimer);
-        };
-    }, 3000)
-    
-//Zadanie 2
-//Po 2 sekundach od uruchomienia programu wyświetl w konsoli wszystkie elementy dowolnej tablicy. Następnie wyświetl w konsoli co 3 sekundy kolejny element tej tablicy.
-
-let myArray =  [1, 2, 3, 4];
-let myTimer = setTimeout(() => {
-        for (let i = 0; i < myArray.length; i++) {
-            console.log(myArray[i]);
-        }
-}, 2000 )
-
-    let j = 0
-    let myInterval = setInterval(() => {
-        console.log(myArray[j]);
-        j++;
-    if (j >= myArray.length) {
-        clearInterval(myInterval);
-    }
-    }, 3000)
-    
+//zadanie 5
+//W konsoli napisz program, który na podstawie wartości zmiennej np. var n = 5; wypisuje wszystkie liczby od zera do n. 
+//Przy każdej liczbie program ma napisać, czy liczba jest parzysta czy nie. Np.:
  
+let value = 10;
+for (let i = 1; i < 11; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " parzysta");
+    }
+    else {
+        console.log(i + " nieparzysta");
+    }
+}
+
+//zadanie 6 
+//W konsoli stwórz dwie pętle niezależne i wypisz wartości ich liczników w każdej iteracji. Wykorzystaj: konkatenację np.
+
+for (i = 0; i <= 3; i++) {
+    for (j = 0; j <= 4; j++) {
+        console.log("i= " + i + ", j= " + j);
+    }
+} 
+
+//zadanie 7
+//FizzBuzz - wypisz w consoli liczby od 0 do 100. Zamiast każdej podzielnej liczby przez 3 wypisz "Fizz", zamiast każdej podzielnej liczby przez 5 wypisz "Buzz" a zamiast 
+//każdej podzielnej liczby przez 3 i przez 5 wypisz "FizzBuzz"
+
+for (let i = 0; i <= 100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("Fizz Buzz")
+    }
+    else if (i % 5 === 0) {
+        console.log("Buzz")
+    }
+    else if (i % 3 === 0) {
+        console.log("Fizz")
+    }
+    else {
+        console.log(i);
+    }
+
+    
+
+}
