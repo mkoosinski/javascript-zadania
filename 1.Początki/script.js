741391fc0879f541ee1d040f0cbfe15922e076a4
mkoosinski <72337644+mkoosinski@users.noreply.github.com>
2021-01-12 00:40:57 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,16 @@
+//zadanie 1
+
+console.log('Mateusz')
+
+//zadanie 2
+
+console.log(12+32)//wynik 44
+console.log(52/16)//wynik 3.25
+console.log(5*2)//wynik 10
+
+//zadanie 3
+
+console.log("Hello World");
+console.log(Hello World)
+console.log("Hello World)
+//Tylko pierwszy console.log się wyświetli  
