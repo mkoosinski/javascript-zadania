7152d25f8d03caa94aced7bcbbc2360fda19b71b
mkoosinski <72337644+mkoosinski@users.noreply.github.com>
2021-01-12 00:49:02 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,27 @@
+// zadanie 1 
+
+let number = 1 
+console.log(number)
+let string = 'string'
+console.log(string)
+let numberAndString = 2 + 'dwa'
+console.log(numberAndString)
+let logic = true
+console.log(logic)
+let special = null
+console.log(special)
+
+//zadanie 2
+
+let firstNumber = 2
+let secondNumber = 4
+let sum = 0
+sum = secondNumber + firstNumber
+console.log(sum)
+
+//zadanie 3
+
+let nothing;
+console.log(nothing)
+
+//wynik undefined ponieważ nie przypisaliśmy wartości do zmienne
