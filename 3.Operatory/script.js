9536eddb8c1a1f19aadc41d3eed47ea64f17eee6
mkoosinski <72337644+mkoosinski@users.noreply.github.com>
2021-01-12 01:03:43 +0100

    completed all tasks

---
+++ 3.Operatory/script.js
@@ -0,0 +1,47 @@
+//zadanie 1
+
+const firstValue = false
+const secondValue = true
+console.log(firstValue === secondValue)
+
+//zadanie 2
+
+let first = 4
+let second = 2 
+let moduloResult = 0
+
+moduloResult = first % second
+console.log(moduloResult);
+
+//zadanie 3
+
+let first = 'first'
+let second = 'second'
+let result = ''
+result = first + second
+console.log(result)
+
+//zadanie 4
+
+var someNumber = 425;
+var someString = '425';
+
+console.log(someNumber == someString) //konwersja typów
+console.log(someNumber === someString) // inny typ zmiennych
+
+//zadanie 5
+
+let counter = 30
+console.log(counter)
+counter++
+console.log(counter)
+counter--
+console.log(counter)
+
+//zadanie 6
+
+let firstNumber = 2
+let secondNumber = 4
+let result = null
+result = firstNumber>secondNumber
+console.log(result)
