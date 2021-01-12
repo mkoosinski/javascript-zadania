d6ea8aa637a7674d533d00afd2a355fcc8908154
mkoosinski <72337644+mkoosinski@users.noreply.github.com>
2021-01-12 01:15:38 +0100

    completed all tasks

--- 4.Pętle i ify/script.js
+++ 4.Pętle i ify/script.js
@@ -1,7 +1,48 @@
+// zadanie 1 
+
+let firstNumber = 1
+let secondNumber = 2
+
+if(firstNumber>secondNumber) {
+  console.log(firstNumber)
+}
+else {
+  console.log(secondNumber)
+}
+
+//zadanie 2
+
+let firstNumber = 1
+let secondNumber = 2
+let thirdNumber = 3
+
+if(firstNumber>secondNumber && firstNumber>thirdNumber) {
+  console.log(firstNumber)
+}
+else if (secondNumber > firstNumber && secondNumber> thirdNumber) {
+  console.log(secondNumber)
+}
+else{
+  console.log(thirdNumber)
+}
+
+//zadanie 3
+
+for(let i = 0; i < 10; i++) {
+  console.log('Lubie JavaScript')
+}
+
+//zadanie 4
+
+let result = 0
+
+for(let i = 0; i<=10; i++) {
+  result = result + i
+}
+console.log(result)
+
 //zadanie 5
-//W konsoli napisz program, który na podstawie wartości zmiennej np. var n = 5; wypisuje wszystkie liczby od zera do n. 
-//Przy każdej liczbie program ma napisać, czy liczba jest parzysta czy nie. Np.:
- 
+
 let value = 10;
 for (let i = 1; i < 11; i++) {
     if (i % 2 === 0) {
@@ -13,7 +54,6 @@ for (let i = 1; i < 11; i++) {
 }
 
 //zadanie 6 
-//W konsoli stwórz dwie pętle niezależne i wypisz wartości ich liczników w każdej iteracji. Wykorzystaj: konkatenację np.
 
 for (i = 0; i <= 3; i++) {
     for (j = 0; j <= 4; j++) {
@@ -22,8 +62,6 @@ for (i = 0; i <= 3; i++) {
 } 
 
 //zadanie 7
-//FizzBuzz - wypisz w consoli liczby od 0 do 100. Zamiast każdej podzielnej liczby przez 3 wypisz "Fizz", zamiast każdej podzielnej liczby przez 5 wypisz "Buzz" a zamiast 
-//każdej podzielnej liczby przez 3 i przez 5 wypisz "FizzBuzz"
 
 for (let i = 0; i <= 100; i++) {
     if(i % 3 === 0 && i % 5 === 0) {
@@ -38,7 +76,4 @@ for (let i = 0; i <= 100; i++) {
     else {
         console.log(i);
     }
-
-    
-
 }
